7f5d28cd15fd5b197c1669e118f1f0f9d6c6f08e
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2025-01-30 22:39:53 -0700

    added more tests body for insertionSort()

--- src/assign04/LargestNumberSolverTest.java
+++ src/assign04/LargestNumberSolverTest.java
@@ -4,47 +4,97 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LargestNumberSolverTest {
-    private Integer[] testArr1, testArr2;
+    private Integer[] testArr1, testArr2, testArrEmpty, testArrSmall, testArrBig;
+    private ArrayList<Integer> bigArrItems;
     private BigInteger bigInteger1, bigInteger2, bigIntegerSum1And2;
-    private List<Integer[]> lists;
+    private ArrayList<Integer[]> lists;
+    private Comparator<Integer> cmp, reverseCmp;
+    private final int bigSize = 10000;
 
     @BeforeEach
     void setup() {
         testArr1 = new Integer[]{4, 1, 8, 3, 7};
         testArr2 = new Integer[]{44, 1, 87, 32, 7};
+        testArrEmpty = new Integer[0];
+        testArrBig = new Integer[bigSize];
         bigInteger1 = new BigInteger("87431");
         bigInteger2 = new BigInteger("87744321");
         bigIntegerSum1And2 = bigInteger1.add(bigInteger2);
-
+        bigArrItems = new ArrayList<>();
         lists = new ArrayList<>();
+
         lists.add(testArr1);
         lists.add(testArr2);
+
+        Random rng = new Random();
+        for (int i = 0; i < bigSize; i++) {
+            bigArrItems.add(rng.nextInt());
+            testArrBig[i] = bigArrItems.get(i);
+        }
+
+        cmp = Comparator.comparingInt(anInt -> anInt);
+        reverseCmp = (int1, int2) -> int2 - int1;
+
+        bigArrItems.sort(cmp);
     }
 
     @Test
-    void testTest() {
-        assertTrue(Arrays.equals(new Integer[]{1, 3, 4, 7, 8}, new Integer[]{1, 3, 4, 7, 8}));
-    }
-    @Test
     void testInsertionSort() {
-        Comparator<Integer> cmp = Comparator.comparingInt(anInt -> anInt);
         LargestNumberSolver.insertionSort(testArr1, cmp);
-        System.out.println(Arrays.toString(testArr1));
-        assertTrue(Arrays.equals(new Integer[]{1, 3, 4, 7, 8}, testArr1));
+        assertArrayEquals(new Integer[]{1, 3, 4, 7, 8}, testArr1);
+    }
+
+    @Test
+    void testInsertionSortEmptyArray() {
+        LargestNumberSolver.insertionSort(testArrEmpty, cmp);
+        assertArrayEquals(new Integer[0], testArrEmpty);
+    }
+
+    @Test
+    void testInsertionSortSmallArray() {
+
+    }
+
+    @Test
+    void testInsertionSortBigArray() {
+        LargestNumberSolver.insertionSort(testArrBig, cmp);
+        assertArrayEquals(bigArrItems.toArray(), testArrBig);
+    }
+
+    @Test
+    void testInsertionSortSorted() {
+
+    }
+
+    @Test
+    void testInsertionSortReversed() {
+
+    }
+
+    @Test
+    void testInsertionSortWithDiffComparator() {
+
+    }
+
+    @Test
+    void testInsertionSortWithDiffComparatorSorted() {
+
+    }
+
+    @Test
+    void testInsertionSortWithDiffComparatorReversed() {
+
     }
 
     @Test
     void testFindLargestNumber() {
-        assertEquals(bigInteger2, LargestNumberSolver.findLargestNumber(testArr2));
+        BigInteger newbe = LargestNumberSolver.findLargestNumber(testArr2);
+        assertEquals(bigInteger2, newbe);
     }
 
     @Test
