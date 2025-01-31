504f44edfbbc588d057e8a47610cddc9e7312360
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2025-01-31 14:15:52 -0700

    added javaDoc and modified the logic chain in findLargestNumber() method

--- src/assign04/LargestNumberSolver.java
+++ src/assign04/LargestNumberSolver.java
@@ -1,12 +1,17 @@
 package assign04;
 
-import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class LargestNumberSolver {
+    /**
+     *
+     * @param arr
+     * @param cmp
+     * @param <T>
+     */
     public static <T> void insertionSort(T[] arr, Comparator<? super T> cmp) {
         // loop over every item in the array
         for (int i = 1; i < arr.length; i++) {
@@ -27,6 +32,11 @@ public class LargestNumberSolver {
         }
     }
 
+    /**
+     *
+     * @param arr
+     * @return
+     */
     public static BigInteger findLargestNumber(Integer[] arr) {
         Comparator<Integer> cmp = (Integer x1, Integer x2) -> {
             ArrayList<Integer> array1 = new ArrayList<>();
@@ -41,32 +51,20 @@ public class LargestNumberSolver {
                 x2 /= 10;
             }
             while (!array1.isEmpty() && !array2.isEmpty()) {
-                // when first index already compare;6,7
-                if (array1.get(0) > array2.get(0)) {
-                    return -1;
-                }
-                if (array1.get(0) < array2.get(0)) {
-                    return 1;
-                }
-                if (array1.get(0).equals(array2.get(0)) && array2.size() == 1 && array1.size() == 1) {
-                    return 0;
-                }
-                // when first index doesn't work;666,667
-                if (array1.get(0).equals(array2.get(0)) && array1.size() > 1 && array2.size() > 1) {
-                    array1.removeFirst();
-                    array2.removeFirst();
-                    continue;
-                }
-                // 6, 665
-                if (array1.get(0).equals(array2.get(0)) && ((array1.size() > 1 && array2.size() == 1) || (array2.size() > 1 && array1.size() == 1))) {
-                    if (array1.size() == 1){
-                        array1.add(array1.get(0));
+                // when the number at the first index is different
+                if (array1.get(0) > array2.get(0)) return -1;
+                else if (array1.get(0) < array2.get(0)) return 1;
+                // when the number at the first index is same and both number have only one digit
+                else if (array1.get(0).equals(array2.get(0)) && array2.size() == 1 && array1.size() == 1) return 0;
+                // when the number at the first index is tie and at least one number have more
+                // than one digit.
+                else {
+                    if (array1.size() == 1) array1.add(array1.get(0));
+                    else if (array2.size() == 1) array2.add(array2.get(0));
+                    else {
+                        array1.removeFirst();
+                        array2.removeFirst();
                     }
-                    else{
-                        array2.add(array2.get(0));
-                    }
-                    array2.removeFirst();
-                    array1.removeFirst();
                 }
             }
             return 0;
@@ -97,6 +95,12 @@ public class LargestNumberSolver {
         return null;
     }
 
+    /**
+     * a helper method to convert an array of integers to a number String
+     *
+     * @param arr an array of integer
+     * @return a String that contains of number
+     */
     private static String makeNumberString(Integer[] arr) {
         StringBuilder bigNumber = new StringBuilder();
         for (Integer integer : arr) bigNumber.append(integer);
