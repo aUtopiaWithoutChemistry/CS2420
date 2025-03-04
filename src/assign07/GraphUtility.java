c9271dfaca67149c4143fc12135b632cabfb5f1a
aUtopiaWithoutChemistry <zuozifan1644@gmail.com>
2025-03-03 22:01:18 -0700

    fixed problem with recognizing srcData and dstData in new generated graph

--- src/assign07/GraphUtility.java
+++ src/assign07/GraphUtility.java
@@ -4,29 +4,28 @@ import java.util.List;
 
 public class GraphUtility {
     public static <Type> boolean areConnected(List<Type> sources, List<Type> destinations, Type srcData, Type dstData) {
-        if (!isInputValid(sources, destinations) || (!sources.contains(srcData) && !destinations.contains(dstData))) {
-            throw new IllegalArgumentException();
-        }
+        if (InputInvalid(sources, destinations)) throw new IllegalArgumentException();
         Graph<Type> graph = buildGraph(sources, destinations);
+        if (!graph.contains(srcData) || !graph.contains(dstData)) throw new IllegalArgumentException();
         return graph.depthFirstSearch(srcData, dstData);
     }
 
     public static <Type> List<Type> shortestPath(List<Type> sources, List<Type> destinations, Type srcData, Type dstData) {
-        if (!isInputValid(sources, destinations)) throw new IllegalArgumentException();
+        if (InputInvalid(sources, destinations)) throw new IllegalArgumentException();
         Graph<Type> graph = buildGraph(sources, destinations);
         if (!areConnected(sources, destinations, srcData, dstData)) throw new IllegalArgumentException();
         return graph.breadthFirstSearch(srcData, dstData);
     }
 
     public static <Type> List<Type> sort(List<Type> sources, List<Type> destinations) {
-        if (!isInputValid(sources, destinations)) throw new IllegalArgumentException();
+        if (InputInvalid(sources, destinations)) throw new IllegalArgumentException();
         Graph<Type> graph = buildGraph(sources, destinations);
         if (graph.isCyclic()) throw new IllegalArgumentException();
         return graph.topoSort();
     }
 
-    private static <Type> boolean isInputValid(List<Type> sources, List<Type> destinations) {
-        return !sources.isEmpty() && sources.size() == destinations.size();
+    private static <Type> boolean InputInvalid(List<Type> sources, List<Type> destinations) {
+        return sources.isEmpty() || destinations.isEmpty() || sources.size() != destinations.size();
     }
 
     private static <Type> Graph<Type> buildGraph(List<Type> sources, List<Type> destinations) {
